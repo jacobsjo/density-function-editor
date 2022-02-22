b46424e7b36d0d8d8fe0adfa685b6453fa7b040a
Jochen Jacobs <JochenJacobs33@gmail.com>
2022-02-22 18:38:08 +0100

    Updates

--- main/UI/MenuManager.d.ts
+++ main/UI/MenuManager.d.ts
@@ -3,7 +3,10 @@ export declare class MenuManager {
     static fileHandle: FileSystemFileHandle;
     static fileName: string;
     static edited: boolean;
+    static save_button: HTMLElement;
     static addHandlers(): void;
+    static save(): Promise<void>;
+    static saveAs(): Promise<void>;
     static setEdited(force?: boolean): void;
 }
 //# sourceMappingURL=MenuManager.d.ts.map
