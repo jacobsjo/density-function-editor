db19fbddcb5a4bd72974719df4259d31d9a336e6
Jochen Jacobs <jochen.jacobs@campus.tu-berlin.de>
2022-06-02 17:12:59 +0200

    Updates

---
+++ 1_18_2/main/nodes/named_density_function.d.ts
@@ -0,0 +1,15 @@
+import { LGraphNodeFixed } from "./LGraphNodeFixed";
+export declare class NamedDensityFunctionNode extends LGraphNodeFixed {
+    static title: string;
+    private wdgt;
+    private has_change;
+    private df;
+    allowMultipleOutputs: boolean;
+    constructor();
+    updateWidgets(): void;
+    private updateColor;
+    getTitle: () => any;
+    onReload(): void;
+    onExecute(): void;
+}
+//# sourceMappingURL=named_density_function.d.ts.map
